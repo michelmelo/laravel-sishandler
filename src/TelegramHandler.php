58cea4f9fdd3904606dde389a8c092f0ba47e29d
Michel Melo <michelmelo@users.noreply.github.com>
2021-10-14 17:23:20 +0100

    log

--- src/TelegramHandler.php
+++ src/TelegramHandler.php
@@ -51,8 +51,8 @@ class sisHandler extends AbstractProcessingHandler
         parent::__construct($level, true);
 
         // define variables for making Telegram request
-        $this->botToken = config('sis-logger.token');
-        $this->chatId   = config('sis-logger.chat_id');
+        $this->apiKey = config('sis-logger.apiKey');
+        $this->apiURL = config('sis-logger.apiURL');
 
         // define variables for text message
         $this->appName = config('app.name');
@@ -64,20 +64,50 @@ class sisHandler extends AbstractProcessingHandler
      */
     public function write(array $record): void
     {
-        if (!$this->botToken || !$this->chatId) {
+        if (!$this->apiKey || !$this->apiURL) {
             return;
         }
 
         // trying to make request and send notification
         try {
-            file_get_contents(
-                'https://api.telegram.org/bot' . $this->botToken . '/sendMessage?'
-                . http_build_query([
-                    'text'       => $this->formatText($record['formatted'], $record['level_name']),
-                    'chat_id'    => $this->chatId,
-                    'parse_mode' => 'html',
-                ])
-            );
+            // file_get_contents(
+            //     'https://api.telegram.org/bot' . $this->botToken . '/sendMessage?'
+            //     . http_build_query([
+            //         'text'       => $this->formatText($record['formatted'], $record['level_name']),
+            //         'chat_id'    => $this->chatId,
+            //         'parse_mode' => 'html',
+            //     ])
+            // );
+            $text                 = $this->formatText($record['formatted'], $record['level_name']);
+            $dataSIS              = new \stdClass();
+            $dataSIS->level       = $record['level_name'];
+            $dataSIS->message     = $text;
+            $dataSIS->description = rtrim(strtok($text, "\n"));
+
+            $dataSIS = json_encode($dataSIS);
+            // Prepare headers
+            $headers = [
+                "Content-Type"   => "application/json",
+                "Content-Length" => strlen($dataSIS),
+                "Authorization"  => $this->apiKey,
+            ];
+
+            foreach ($headers as $header => &$content) {
+                $content = "{$header}: {$content}";
+            }
+
+            $headers = array_values($headers);
+
+            // Send the request
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $this->apiURL);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $dataSIS);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HEADER, true);
+            curl_exec($curl);
+            curl_close($curl);
         } catch (Exception $exception) {
 
         }
