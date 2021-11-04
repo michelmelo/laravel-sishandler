f47b41c85e712b9376367347e32dee7978eb32e2
Michel Melo <michelmelo@users.noreply.github.com>
2021-11-04 12:55:03 +0000

    clean

--- src/SisHandler.php
+++ src/SisHandler.php
@@ -12,33 +12,10 @@ use Monolog\Logger;
  */
 class SisHandler extends AbstractProcessingHandler
 {
-    /**
-     * Bot API token
-     *
-     * @var string
-     */
-    private $botToken;
 
-    /**
-     * Chat id for bot
-     *
-     * @var int
-     */
-    private $chatId;
+    private $apiKey;
 
-    /**
-     * Application name
-     *
-     * @string
-     */
-    private $appName;
-
-    /**
-     * Application environment
-     *
-     * @string
-     */
-    private $appEnv;
+    private $apiURL;
 
     /**
      * SisHandler constructor.
@@ -50,13 +27,9 @@ class SisHandler extends AbstractProcessingHandler
 
         parent::__construct($level, true);
 
-        // define variables for making Telegram request
         $this->apiKey = config('sis-logger.apiKey');
         $this->apiURL = config('sis-logger.apiUrl');
 
-        // define variables for text message
-        $this->appName = config('app.name');
-        $this->appEnv  = config('app.env');
     }
 
     /**
@@ -74,9 +47,9 @@ class SisHandler extends AbstractProcessingHandler
             $dataSIS->level   = $record['level'];
             $dataSIS->message = $record['message'];
             $dataSIS->context = $record;
-            
+
             $dataSIS = json_encode($dataSIS);
-            $url = $this->apiURL . '?appKey=' . $this->apiKey;
+            $url     = $this->apiURL . '?appKey=' . $this->apiKey;
             $headers = ['Content-Type: application/json'];
 
             $ch = curl_init();
@@ -92,6 +65,5 @@ class SisHandler extends AbstractProcessingHandler
 
         }
     }
-
 
 }
